6ad5fd4386c3217397ac5a2ca4950fe85e66f717
Anthony <anthony@ejemplo.com>
2025-10-26 22:42:21 -0500

    Fix middleware headers order

--- DSW_PROYECTO_PALACIO_CAMISAS_WebApp/Program.cs
+++ DSW_PROYECTO_PALACIO_CAMISAS_WebApp/Program.cs
@@ -2,11 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddDistributedMemoryCache(); // Agrega esta línea
-
+builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
-
 
 var app = builder.Build();
 
@@ -14,7 +11,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -23,17 +19,18 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.Use(async (context, next) => // Middleware para deshabilitar el caché
+// Middleware CORREGIDO - poner ANTES de next()
+app.Use(async (context, next) =>
 {
-    await next();
-
+    // Configurar headers ANTES de ejecutar el siguiente middleware
     context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
     context.Response.Headers["Pragma"] = "no-cache";
     context.Response.Headers["Expires"] = "0";
+
+    await next(); // Ahora sí ejecutar el siguiente middleware
 });
 
 app.UseSession();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
